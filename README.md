8208e9fad5e590f1b53303ef1fa942dd5a90c1ef
Sylvain Reucherand <sylvain.reucherand@gmail.com>
2022-06-09 15:13:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @pretto/prettier-config-pretto
+
+[![npm version](https://img.shields.io/npm/v/@pretto/prettier-config-pretto.svg?style=flat)](https://www.npmjs.com/package/@pretto/prettier-config-pretto)
+
+A shareable Prettier configuration.
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  // @prettier/plugin-php
+  phpVersion: '7.4',
+  // @trivago/prettier-plugin-sort-imports
+  importOrder: [
+    '^@pretto/bricks/(.*)$',
+    '^@pretto/zen/(.*)$',
+    '^@pretto/app-core/(.*)$',
+    '^@pretto/picto/(.*)$',
+    '^@pretto/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^../(.*(?!(css)))$',
+    '^./(.*)$',
+    '^(?![.]).*(.css)$',
+    '^.*(.css)$',
+  ],
+  importOrderSeparation: true,
+}
