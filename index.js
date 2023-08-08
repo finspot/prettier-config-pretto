257969abe84981f57da5d799c157af01cd91a50a
Sylvain Reucherand <sreucherand@users.noreply.github.com>
2023-08-08 16:41:03 +0200

    [fix] prettier 3 plugins (#2)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSpacing: true,
+  plugins: ['@prettier/plugin-php', '@trivago/prettier-plugin-sort-imports'],
   printWidth: 120,
   semi: false,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: false,
+  // @prettier/plugin-php
+  phpVersion: '7.4',
   // @trivago/prettier-plugin-sort-imports
   importOrder: [
     '^@pretto/bricks/(.*)$',
